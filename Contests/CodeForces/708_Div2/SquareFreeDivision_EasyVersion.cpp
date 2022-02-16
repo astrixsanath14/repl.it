d6acfd480bcd4affd1a74afdd0bb24ca5bbf8105
sanath.rs <sanath.rs@zohocorp.com>
2022-02-16 21:08:07 +0530

    sync

--- Contests/CodeForces/708_Div2/SquareFreeDivision_EasyVersion.cpp
+++ Contests/CodeForces/708_Div2/SquareFreeDivision_EasyVersion.cpp
@@ -1,50 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<int> primeFactors(int n)
 {
-    int c=2;
-    vector<int> primes;
-    while(n>1)
+  int c = 2;
+  vector<int> primes;
+  while (n > 1)
+  {
+    if (n % c == 0)
     {
-        if(n%c==0){
-        cout<<c<<" ";
-        n/=c;
-        }
-        else c++;
+      cout << c << " ";
+      n /= c;
     }
+    else
+      c++;
+  }
 }
 
 void optimizeArray(int a[])
 {
-  for(int i=0; sizeof(a)/sizeof(int);i++)
+  for (int i = 0; sizeof(a) / sizeof(int); i++)
   {
-    while(a[i])
+    while (a[i])
     {
-      a[i]
+      a[i];
     }
   }
 }
-
 void solve()
 {
-  int n,k;
-  cin>>n>>k;
+  int n, k;
+  cin >> n >> k;
   int a[n];
-  for(int i=0;i<n;i++)
+  for (int i = 0; i < n; i++)
   {
-    cin>>a[i];
+    cin >> a[i];
   }
-
 }
-
 int main()
 {
   int t;
-  cin>>t;
-  while(t--)
+  cin >> t;
+  while (t--)
   {
     solve();
   }
   return 0;
 }
+
+/*
+Contests/CodeForces/708_Div2/SquareFreeDivision_EasyVersion.cpp
+*/
