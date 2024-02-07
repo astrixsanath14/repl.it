30be745cc4b08f9c59ec18b46d92a19fe42cf196
astrixsanath14 <astrixsanath14@gmail.com>
2024-02-07 10:20:43 +0000

    sync from repl

---
+++ OA/G/UncleNephewGifts.py
@@ -0,0 +1,20 @@
+def maximum_gifts(N, gifts):
+  gifts.sort(key=lambda x: (x[0], x[1]))
+  max_gifts = 0
+  budget = 0
+  for i in range(N):
+      budget += gifts[i][0]
+      if i > 0:
+          budget -= gifts[i-1][0]
+      if gifts[i][1] <= budget:
+          budget -= gifts[i][1]
+          max_gifts += 1
+  return max_gifts
+  
+gifts = [
+(3, 2),
+(5, 4),
+(6, 3)
+]
+result = maximum_gifts(N, gifts)
+print(result)
